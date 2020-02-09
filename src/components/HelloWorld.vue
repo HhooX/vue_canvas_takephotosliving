230803094f2bd060874286908499d5099137654a
volodya <18356090933@163.com>
2020-02-09 17:15:00 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <takephoto/>
+  </div>
+</template>
+
+<script>
+import takephoto from './takephoto'
+export default {
+  name: "HelloWorld",
+  components:{
+    takephoto
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import "@/styles/global.scss";
+.hello {
+  display: flex;
+  flex-flow: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  width: vw(1920);
+  height: vh(1080);
+  overflow: hidden;
+}
+a {
+  color: #42b983;
+  color: #2c3e50;
+}
+</style>
